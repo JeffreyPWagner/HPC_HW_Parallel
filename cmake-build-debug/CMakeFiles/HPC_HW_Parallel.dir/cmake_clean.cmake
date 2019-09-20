d174aa09db410e959f905394c834a54bb09cb0a0
JeffreyPWagner <jeffpwagner@gmail.com>
2019-09-19 20:04:25 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/HPC_HW_Parallel.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPC_HW_Parallel.dir/main.cpp.obj"
+  "HPC_HW_Parallel.pdb"
+  "HPC_HW_Parallel.exe"
+  "HPC_HW_Parallel.exe.manifest"
+  "HPC_HW_Parallel.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HPC_HW_Parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
